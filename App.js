bf8e259759924d0247dff6c4e699b846e53f5925
Faiza Aftab <102207534+faiza-ahmad@users.noreply.github.com>
2022-09-21 00:36:19 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,17 @@
+const contents=document.querySelectorAll('.content');
+const listitems=document.querySelectorAll('nav ul li')
+listitems.forEach((item,idx)=>{
+   item.addEventListener('click',()=>{
+       hideAllContents()
+       hideAllItems()
+       item.classList.add('active')
+       contents[idx].classList.add('show')
+   })
+})
 
+function hideAllContents(){
+    contents.forEach(content=> content.classList.remove('show'))
+}
+function hideAllItems(){
+    contents.forEach(item=> item.classList.remove('active'))
+}
